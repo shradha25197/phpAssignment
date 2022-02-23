91ce090b0bff8766c01cfacad0f287b830e280eb
shradha25197 <100135044+shradha25197@users.noreply.github.com>
2022-02-23 20:53:07 +0530

    Add files via upload

---
+++ currencyConvertor/userInfo.php
@@ -0,0 +1,85 @@
+<?php
+include "dbConnection.php";
+include "header.php";
+?>
+<html>
+<head>
+<link rel="stylesheet"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+	crossorigin="anonymous">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script>
+
+function deleteUser(user)
+{
+ 				$.ajax({
+ 				url:"ajax.php",
+ 				type:"POST",
+ 				data:{"userInfo":user},
+ 				success: function(data) {
+ 				if(data == "SUCCESS")
+ 				{
+ 				alert("User Deleted Successfully");
+ 				location.reload();
+ 				}
+ 				else
+ 				{
+ 				alert("User Deleted Failure");
+ 				}
+ 				},
+ 				error: function (error)
+ 				{
+ 				alert("by");
+ 				}
+ 				
+                });
+}
+</script>
+</head>
+<body>
+<?php
+
+$sql = "SELECT * FROM USER_MASTER";
+
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    ?>
+     <table border="1">
+
+		<tbody>
+
+<?php
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        ?>
+<tr>
+				<td>
+	<?php echo $row['USER_ID']; ?>
+    
+</td>
+				<td>
+	<?php echo $row['USER_NAME']; ?>
+    
+</td>
+				<td>
+	<?php echo $row['USER_EMAIL']; ?>
+    
+</td>
+				<td>
+					<button class="form-control" style="background-color: #1eb37c;"
+						onclick="deleteUser('<?php echo $row['USER_ID']; ?>')">Delete</button>
+
+				</td>
+			</tr>
+   <?php }?>
+</tbody>
+	</table>
+    <?php
+}
+?>
+
+</body>
+</html>
